f425488c9242c11729270192ede0fd935eb75a6f
nxcoo <puppo.nicholas@gmail.com>
2022-03-01 22:09:41 +0100

    aggiunta home

---
+++ SHADOW RP/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/IMG/logo.png" type="image/x-icon">
+    <title>Shadow RP - Home</title>
+  </head>
+  <body>
+    <header>
+      <div class="inner-width">
+        <a href="#" class="logo"><img src="/img/logo.gif" alt=""></a>
+        <i class="menu-toggle-btn fas fa-bars"></i>
+        <nav class="navigation-menu">
+          <strong>
+          <a href="#"></i> Home</a>
+          <a href="#"></i> Staff</a>
+          <a href="#"></i> Pacchetti</a>
+          <a href="#"></i> History</a>
+          <a href="#"></i> Changelog</a>
+          </strong>
+        </nav>
+      </div>
+    </header>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="part1">
+      <hr id="line">
+      <h1 id="text-title">SHADOW RP</h1>
+     <hr id="line1">
+     <br>
+     <h1 id="text-welcomesite">
+     Ti diamo il Benvenuto nel sito ufficiale del nostro server rp,
+      scorrendo nelle varie sezioni puoi trovare tutti i nostri indirizzi social e le informazioni necessare per entrare a far parte della nostra community,
+     tutti gli aggiornamenti e le novità possono essere raggiunte facilmente grazie a questo sito semplice ed intuitivo,
+     ma ricorda di entrare sui nostri server discord per essere sempre aggiornato 24h sugli eventi ed ottenere accesso illimitato al nostro server FIVEM.
+      </h1>
+    </div>
+    <br>
+
+      <h1 id="number-rp"><strong>I NUMERI DI SHADOW ROLEPLAY</strong></h1>
+      <br>
+      <hr id="hr-number">
+      <br>
+        <!-- SLOT SERVER !-->
+        <p class="n°1">256</p>
+        <p class="n°2">SLOT SERVER</p>
+        <!-- UTENTI NEL DISCORD !-->
+        <p class="n°3">400</p>
+        <p class="n°4">UTENTI NEL DISCORD</p>
+        <!-- PLAYERS CONNESSI INSIEME !-->
+        <p class="n°5">60</p>
+        <p class="n°6">PLAYERS CONNESSI INSIEME</p>
+      <div class="regolamento">
+        <!-- TESTO !-->
+        <h1 class="regolamento-1">Inizia la tua storia su SHADOW RP</h1>
+        <p class="regolamento-2">
+          Vivi la tua storia con decisioni importanti da fare nel tempo della tua vita. <br>
+          Scopri il tuo potenziale tramite il tuo personaggio... <br>
+          Leggi attentamente il regolamento...
+        </p>
+         <img class="img_regolamento" src="/img/regolamento.jpg" alt=""> 
+      </div>
+
+      <br>
+      <br>
+
+      <div class="accordion">
+  <div>
+    <input type="radio" name="example_accordion" id="section1" class="accordion__input">
+    <label for="section1" class="accordion__label">Come posso entrare nel server? 📌</label>
+    <div class="accordion__content">
+      <p>
+      Per giocare a Imperium basta scaricare <a href="https://fivem.net">Fivem</a>, entrare nel nostro discord e nella chat ooc fare il seguente comando <code style="font-size: 25px;">!ip</code>
+      </p>
+    </div>
+  </div>
+  <div>
+    <input type="radio" name="example_accordion" id="section2" class="accordion__input">
+    <label for="section2" class="accordion__label">Come posso collegarmi al Teamspeak? 🔊</label>
+    <div class="accordion__content">
+      <p>
+      Ti basterà scaricare <strong>Teamspeak</strong> e scaricare la versione che c'è sul nostro discord nella sezione <code style="font-size: 15px;">📝・𝗦𝗔𝗟𝗧𝗬𝗖𝗛𝗔𝗧</code>
+      </p>
+    </div>
+  </div>
+  <div>
+    <input type="radio" name="example_accordion" id="section3" class="accordion__input">
+    <label for="section3" class="accordion__label">Come posso ricevere supporto? ⚠️</label>
+    <div class="accordion__content">
+      <p>
+      Entrando nel nostro discord, potrai ricevere assistenza, compilando prima il modulo richiesto poi fare il comando !assistenza nella sezione apposita e infine recati in attesa assistenza.
+      </p>
+    </div>
+  </div>
+</div>
+
+      <br>
+      <br>
+
+    <!-- FOOTER !-->
+
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>I nostri servizi:</h3>
+                        <ul>
+                            <li><a href="https://discord.gg/FjhvpaH">Community Shadow</a></li>
+                            <li><a href="https://discord.gg/shadowrp">Shadow RP Discord</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Social:</h3>
+                        <ul>
+                            <li><a href="https://www.instagram.com/shadow.rp.ita/">Instagram</a></li>
+                            <li><a href="https://www.tiktok.com/@shadow.rp.ita">Tik Tok</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Shadow RP</h3>
+                        <p>Due community differenti, una dedicata a tutti i giochi di cui siete interessati e l'altra interamente dedicata al nostro server di FIVEM.<br>
+                          I link ai due rispettivi server potete trovarli nella sezione <strong>"I nostri servizi"</strong>.</p>
+                    </div>
+                </div>
+                <p class="copyright">© 2022 Copyright Shadow RP All Rights Reserved <br>
+                <strong>Developed by Ertedesco</strong>
+              </p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <!-- END FOOTER !-->
+
+    <script src="/js/menu.js"></script>
+    <script src="/js/test.js"></script>
+  </body>
+</html>
